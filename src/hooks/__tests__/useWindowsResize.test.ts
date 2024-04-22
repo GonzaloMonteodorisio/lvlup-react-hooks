4cdc9ecfe0fc1a3f43d4031bc52611933fadf51d
Franco Di Leo <dileo.francoj@gmail.com>
2024-04-22 10:42:06 -0300

    chore: export correct hooks

--- src/hooks/__tests__/useWindowsResize.test.ts
+++ src/hooks/__tests__/useWindowsResize.test.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { fireEvent } from '@testing-library/react'
-import { act, renderHook } from '@testing-library/react-hooks'
-import { beforeEach, describe, expect, it, type MockInstance, vi } from 'vitest'
+import { renderHook } from '@testing-library/react-hooks'
+import { beforeEach, describe, expect, it } from 'vitest'
 
 import { useWindowsResize } from '../useWindowsResize'
 
 describe('useWindowsResize', () => {
-  let addEventListenerMock: MockInstance
-  let removeEventListenerMock: MockInstance
+  // let addEventListenerMock: MockInstance
+  // let removeEventListenerMock: MockInstance
 
   beforeEach(() => {
     global.innerHeight = 1024
     global.innerWidth = 600
-    addEventListenerMock = vi.fn(window.addEventListener)
-    removeEventListenerMock = vi.fn(window.removeEventListener)
+    // addEventListenerMock = vi.fn(window.addEventListener)
+    // removeEventListenerMock = vi.fn(window.removeEventListener)
   })
 
   it('should update windows size on resize', () => {
@@ -27,10 +27,4 @@ describe('useWindowsResize', () => {
     expect(result.current).toEqual({ width: 1024, height: 610 })
   })
   // probar que se debe remover el event listener cuando el componente se desmonta
-  it('should add and remove event listener on mount and unmount', () => {
-    const { unmount } = renderHook(() => useWindowsResize())
-    // expect(addEventListenerMock).toHaveBeenCalledWith('resize', expect.any(Function))
-    // una vez desmontado el componente
-    // se llame removeEventListenerMock con la función de callback
-  })
 })
--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,14 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({outDir: 'dist', include: ["src/"]})],
+  //   plugins: [dts({ outDir: "dist", include: ["src/"], exclude: ["**/__tests__/**"] }), tsconfigPaths()],
+
+  plugins: [react(), dts({ outDir: 'dist', include: ["src/index.ts"], exclude: ["src/**/__tests__/**"] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'lvlup-react-hooks',
-      formats: ['es','umd']
+      formats: ['es', 'umd']
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
